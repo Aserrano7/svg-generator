72d0f46dcf13f7fbc05ccddfb7ecad3e8fbb7acf
Aserrano7 <serranoandres022@gmail.com>
2023-12-11 15:01:08 -0800

    added svg class with methods

--- index.js
+++ index.js
@@ -30,3 +30,20 @@ const questions = [
         message: "Enter a shape color",
     },
 ];
+
+class SVG{
+    constructor(){
+        this.text1 ='';
+        this.shape1 ='';
+    }
+    render(){
+        return `<svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="300" height="200">${this.shape1}${this.text1}</svg>`;
+    }
+    setText(text,color){
+        this.text1=`<text x="150" y="125" font-size="60" text-anchor="middle" fill="${color}">${text}</text>`
+    }
+    setShape(shape){
+        this.shape1 = shape.render();
+    }
+
+}
